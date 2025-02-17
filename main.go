cdaa9da53ada751747ec887335160037f11b4a70
wangshengmu <wangshengmu@bytedance.com>
2025-02-17 19:15:32 +0800

    init

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello")
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":5001")
 
 }
