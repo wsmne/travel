1a408621c37fa1043b5736befa4697e8c43c8b8d
wangshengmu <wangshengmu@bytedance.com>
2025-03-04 14:07:03 +0800

    init

--- handler.go
+++ handler.go
@@ -6,3 +6,17 @@ import (
 	"proj/travel/controllers"
 	"proj/travel/middleware"
 )
+
+func RegisterRouter(r *gin.Engine) {
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "ok",
+		},
+		)
+	})
+	r.POST("/user", controllers.Regist)
+	r.GET("/login", controllers.Login)
+	r.Use(middleware.ParseToken)
+	r.PUT("/user", controllers.UpdateUser)
+
+}
--- main.go
+++ main.go
@@ -2,24 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"proj/travel/controllers"
-	"proj/travel/middleware"
 )
-
-func RegisterRouter(r *gin.Engine) {
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "ok",
-		},
-		)
-	})
-	r.POST("/user", controllers.Regist)
-	r.GET("/login", controllers.Login)
-	r.Use(middleware.ParseToken)
-	r.PUT("/user", controllers.UpdateUser)
-
-}
 
 func main() {
 
