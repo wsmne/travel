d9c5e11a1c534eb1fa2948f1119326798ba776f7
wangshengmu <wangshengmu@bytedance.com>
2025-04-07 00:41:24 +0800

    add

--- models/scene.go
+++ models/scene.go
@@ -1,6 +1,8 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Scene struct {
 	gorm.Model
@@ -8,5 +10,7 @@ type Scene struct {
 	City     string `gorm:"type:varchar(20);not null" json:"city"`
 	Province string `gorm:"type:varchar(20);not null" json:"province"`
 	Price    string `gorm:"type:varchar(20)"  json:"price"`
-	Image    string `gorm:"type:varchar;not null" json:"image"`
+	Image    string `gorm:"type:text;not null" json:"image"`
+	Goods    int    `gorm:"type:int;not null" json:"goods"`
+	Views    int    `gorm:"type:int;not null" json:"views"`
 }
--- models/score.go
+++ models/score.go
@@ -1,14 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Score struct {
 	gorm.Model
-	UserId     int    `gorm:"not null" json:"user_id"`
-	SceneId    int    `gorm:"not null" json:"scene_id"`
-	Score      int    `gorm:"not null" json:"score"`
-	ScoreType  int    `gorm:"not null" json:"score_type"`
-	ScoreTime  string `gorm:"not null" json:"score_time"`
-	ScoreDesc  string `json:"score_desc"`
-	ScoreState int    `json:"score_state"`
+	UserId     uint    `gorm:"not null" json:"user_id"`
+	SceneId    uint    `gorm:"not null" json:"scene_id"`
+	Score      float64 `gorm:"not null" json:"score"`
+	ScoreType  int     `gorm:"not null" json:"score_type"`
+	ScoreTime  string  `gorm:"not null" json:"score_time"`
+	ScoreDesc  string  `json:"score_desc"`
+	ScoreState int     `json:"score_state"`
+}
+
+func GetScoreByUserId(c *gin.Context) {
+
 }
